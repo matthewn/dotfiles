80e2696d4c312d927bb2a51d12cb415111b94b5c
Matthew Newton <matthewn@berkeley.edu>
2025-08-29 12:30:42 -0700

    recast eslint.config.js to work with older node versions

--- .config/eslint/eslint.config.js
+++ .config/eslint/eslint.config.js
@@ -1,21 +1,21 @@
 // eslint.config.js
-export default [
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
     {
         languageOptions: {
             ecmaVersion: 2024,
             sourceType: 'module',
             globals: {
-                // add any global variables you use
                 console: 'readonly',
-                process: 'readonly'
-            }
+                process: 'readonly',
+            },
         },
         rules: {
             'no-unused-vars': 'error',
             'no-console': 'off',
             'indent': ['error', 4],
             'quotes': ['error', 'single'],
-            'semi': ['error', 'always']
-        }
-    }
+            'semi': ['error', 'always'],
+        },
+    },
 ];
